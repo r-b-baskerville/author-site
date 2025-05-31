92ab1ffeb8c504db574d9907cc7f7e4d9a17c9f3
r-b-baskerville <rebecca.baskerville@outlook.com>
2025-05-31 16:35:28 +0100

    Initial site framework

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const track = document.querySelector(".carousel-track");
+    const slides = Array.from(track.children);
+    const prevButton = document.querySelector(".carousel-button.prev");
+    const nextButton = document.querySelector(".carousel-button.next");
+
+    let currentIndex = 0;
+
+    function updateCarousel() {
+        const slideWidth = slides[0].getBoundingClientRect().width;
+        track.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
+    }
+
+    nextButton?.addEventListener("click", () => {
+        console.log("Next clicked")
+        currentIndex = (currentIndex + 1) % slides.length;
+        updateCarousel();
+    })
+
+    prevButton?.addEventListener("click", () => {
+        console.log("prev clicked")
+        currentIndex = (currentIndex - 1) % slides.length;
+        updateCarousel();
+    })
+
+    updateCarousel();
+})
